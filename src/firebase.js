5e5e412cd98c21dee2ef9004eb45620cf36ffee8
Don-Alvin <alvindon41@gmail.com>
2023-01-04 16:28:13 +0300

    Redux tool kit installed

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyAhdhjehIKXuJ84KdZRDJ3oMlJhge9qLRs",
+	authDomain: "netflix-react-cde3d.firebaseapp.com",
+	projectId: "netflix-react-cde3d",
+	storageBucket: "netflix-react-cde3d.appspot.com",
+	messagingSenderId: "722117292089",
+	appId: "1:722117292089:web:f6058959aa6029ba895de8",
+};
